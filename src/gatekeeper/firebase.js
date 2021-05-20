66207c5ae2c5f96ecb15afb78d9a0a2e2bba7b2f
Shriji <shrijike@gmail.com>
2021-05-20 22:59:46 +0530

    remove firebaseConfig

--- src/gatekeeper/firebase.js
+++ src/gatekeeper/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase/compat/app";
 
 import "firebase/compat/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyDU0m7nq2IUoMuYzsq-qVGGVzaukMku7SU",
-  authDomain: "mascot-project.firebaseapp.com",
-  projectId: "mascot-project",
-  storageBucket: "mascot-project.appspot.com",
-  messagingSenderId: "164321183697",
-  appId: "1:164321183697:web:8cf554e1896f216fda42f0",
-  measurementId: "G-YN83EFHH86",
+ //firebase config
 };
 
 import { authState, currentSession } from "../stores/auth";
